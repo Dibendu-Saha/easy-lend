c07d89754630dfa10b1c34926de607381d15fe2e
Lavish <lavishaggarwal91@yahoo.in>
2024-07-28 18:30:58 +0530

    fixed design issues

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,10 @@ const Header = () => {
     return (
         <nav className="navbar navbar-default navbar-trans navbar-expand-lg fixed-top">
             <div className="container">
-                <a className="navbar-brand text-brand" style={{ fontSize: '3.2rem' }} href="index.html">Urban<span className="color-b">Housing</span></a>
+                <a className="navbar-brand text-brand"
+                    style={{ fontSize: '3.2rem' }}
+                    href="file:///Users/lavishaggarwal/Desktop/UrbanHousing/index.html">Urban<span className="color-b">Housing</span>
+                </a>
             </div>
         </nav>
     )

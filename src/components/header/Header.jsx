eeecce1835f1c7bcafa5993f47c673778313ac30
Dibendu Saha <dibendu4@gmail.com>
2024-07-30 18:14:03 +0530

    Remove link from logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,10 +2,11 @@ const Header = () => {
     return (
         <nav className="navbar navbar-default navbar-trans navbar-expand-lg fixed-top">
             <div className="container">
-                <a className="navbar-brand text-brand"
+                <div className="navbar-brand text-brand"
                     style={{ fontSize: '3.2rem' }}
-                    href="file:///Users/lavishaggarwal/Desktop/UrbanHousing/index.html">Urban<span className="color-b">Housing</span>
-                </a>
+                >
+                    Urban<span className="color-b">Housing</span>
+                </div>
             </div>
         </nav>
     )

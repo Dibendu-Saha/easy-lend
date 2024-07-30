4ea3915698168a83c8eaa2d36c7804c9a9361f6b
Dibendu Saha <dibendu4@gmail.com>
2024-07-30 12:33:20 +0530

    Disable button when no files uploaded

--- src/components/uploadDocuments/UploadDocuments.jsx
+++ src/components/uploadDocuments/UploadDocuments.jsx
@@ -63,7 +63,7 @@ const UploadDocuments = ({ initialValues, responseData, moveToNextStep = () => {
                 </div>
             </div>
             <div className="form-group button-group" style={{ marginTop: 50 }}>
-                <Button variant="success" className="button" onClick={() => onSubmit()}>
+                <Button variant="success" className="button" onClick={() => onSubmit()} disabled={!files.length}>
                     Submit
                 </Button>
             </div>
